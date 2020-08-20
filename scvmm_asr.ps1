ba7d9fe38b4d9aac09d1adb30c64883e9459faf4
Hemanth Vemulapalli <hemanthvemulapalli@gmail.com>
2020-08-20 09:02:39 -0700

    Create scvmm_asr.ps1

---
+++ scvmm_asr.ps1
@@ -0,0 +1,37 @@
+$uname = <<"service principal App ID">>
+$passwd = ConvertTo-SecureString -String <<"Service Principal secret">> -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $uname, $passwd
+Connect-AzAccount -ServicePrincipal -Credential $Credential -Tenant <<tenant ID>>
+$context = Get-AzContext
+$vault = Get-AzRecoveryServicesVault -Name <<'vault name'>>
+$rg = Get-AzResourceGroup -Name $vault.ResourceGroupName
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $rg.ResourceGroupName
+$saId = Get-AzResource -Name $storageAccount.StorageAccountName -ResourceGroupName $rg.ResourceGroupName
+Set-AzRecoveryServicesAsrVaultContext -Vault $vault
+$fabric = Get-AzRecoveryServicesAsrFabric -FriendlyName <<'ScVMM Server name in ASR'>>
+$protectionContainer = Get-AzRecoveryServicesAsrProtectionContainer -Fabric $fabric
+$containerMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $protectioncontainer 
+$vm = Get-SCVirtualMachine | Where { $_.cloud.name -eq <<'SCVMM Cloud name'>> }
+Write-Host "VM "+ $vm.Name
+$vm | ForEach-Object {
+    Write-Host "tag for VM "+ $_.Name +" is "+ $_.tag
+    $protectableItem = Get-AzRecoveryServicesAsrProtectableItem -ProtectionContainer $protectioncontainer -FriendlyName $_.Name
+	If($_.tag -like "*Critical*") {
+        Write-Host "Tag is critical for "+ $_.Name
+		
+        Write-Host "inside if Protection status is "+ $protectableItem.ProtectionStatus
+		If( $protectableItem.ProtectionStatus -eq "Unprotected") {
+			Write-Host "inside if enabling Protection"
+			New-AzRecoveryServicesAsrReplicationProtectedItem -HyperVToAzure -ProtectableItem $protectableItem -ProtectionContainerMapping $containerMapping -Name $protectableItem.Name -OS $protectableItem.OS -RecoveryAzureStorageAccountId $said.ResourceId -OSDiskName $protectableItem.Disks.Name -RecoveryResourceGroupId $rg.ResourceId
+		}
+	}else{
+        Write-Host "inside else "+ $_.Name +" tag "+ $_.tag
+		
+        Write-Host "Inside else Protection status is "+ $protectableItem.ProtectionStatus
+		If( $protectableItem.ProtectionStatus -eq "Protected") {
+			Write-Host "inside if disabling Protection"
+			$protectedItem = Get-AzRecoveryServicesAsrReplicationProtectedItem -ProtectionContainer $protectioncontainer -FriendlyName $_.Name
+			Remove-AzRecoveryServicesAsrReplicationProtectedItem -ReplicationProtectedItem $protectedItem
+		}
+	}
+}
